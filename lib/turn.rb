f998714a6e67b0ee1ebe312b0f19fac43f978de9
Jeff Johnson <synotik@hotmail.com>
2020-09-18 04:51:44 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,27 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  if index < 0
+    return false
+  elsif index > 8
+    return false
+  elsif position_taken?(board, index)
+    return false
+  else
+    return true
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
